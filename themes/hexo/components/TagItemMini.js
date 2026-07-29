15bdcfae7b93c392370b5a8358bb2d2d40151517
cryfishr-hue <cryfishr@gmail.com>
2026-07-29 17:40:11 +0800

    Update TagItemMini.js

--- themes/hexo/components/TagItemMini.js
+++ themes/hexo/components/TagItemMini.js
@@ -3,19 +3,13 @@ import SmartLink from '@/components/SmartLink'
 const TagItemMini = ({ tag, selected = false }) => {
   return (
     <SmartLink
-      key={tag}
-      href={selected ? '/' : `/tag/${encodeURIComponent(tag.name)}`}
-      passHref
-      className={`cursor-pointer inline-block rounded hover:bg-indigo-400 dark:hover:text-white  hover:text-white duration-200
-        mr-2 py-0.5 px-1 text-xs whitespace-nowrap 
-         ${selected
-        ? 'text-white dark:text-gray-300 bg-black dark:bg-black dark:hover:bg-indigo-900'
-        : `text-gray-600 hover:shadow-xl dark:border-gray-400 notion-${tag.color}_background `}` }>
-
-      <div className='font-light'>{selected && <i className='mr-1 fa-tag'/>} {tag.name + (tag.count ? `(${tag.count})` : '')} </div>
-
-    </SmartLink>
-  );
+  key={tag}
+  href={selected ? '/' : `/tag/${encodeURIComponent(tag.name)}`}
+passHref
+className={`cursor-pointer inline-block rounded-md duration-200 mr-2 mb-1 py-1 px-2 text-xs whitespace-nowrap hover:bg-indigo-400 hover:text-white ${selected ? 'text-white bg-black' : 'text-gray-700 bg-gray-100 dark:bg-gray-800 dark:text-gray-300'}`}>
+<div className='font-medium'>#{tag.name}{tag.count ? `(${tag.count})` : ''}</div>
+  </SmartLink>
+)
 }
 
 export default TagItemMini
